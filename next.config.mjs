e45e46c91605667b84d802e850782e8ddf2fcf66
joshimanshu <joshimanshu14@gmail.com>
2024-04-21 15:37:56 +0530

    update next.js config2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV ==='production';
+const isProd = process.env.NODE_ENV = 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/portfolio' : '',
+    basePath: '/portfolio',
     tagret: 'static',
     distDir:"dist",
     images:{
